47b8e0ab247405f014087fdd5ac601a6793042a8
immanojraj <77238730+immanojraj@users.noreply.github.com>
2023-05-15 15:08:52 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,11 +21,12 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  */
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
+  const p = main.querySelector('p');
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+  if (h1 && picture && p && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [h1, picture] }));
+    section.append(buildBlock('hero', { elems: [h1, p, picture] }));
     main.prepend(section);
   }
 }
